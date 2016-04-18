4b3b4a40413848e2d0069599270128616e6f6404
zaim ramlan <zaem.muz@gmail.com>
2016-04-18 13:37:32 +0800

    modified view

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,5 +2,9 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
 
   validates :content, 	presence: true,
-  						length: { minimum: 2 }
+  											length: { minimum: 2 }
+
+  def completed?
+  	!completed_at.blank?
+  end
 end
